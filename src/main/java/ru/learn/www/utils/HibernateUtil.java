5142d9a5f4c69799dcca6f609606ab28ece57ad5
anton <anton.it.pavlov@gmail.com>
2016-10-24 02:11:34 +0300

    commit

--- src/main/java/ru/learn/www/utils/HibernateUtil.java
+++ src/main/java/ru/learn/www/utils/HibernateUtil.java
@@ -8,27 +8,34 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 // название класса может быть любым
 public class HibernateUtil {
 
-    private static SessionFactory sessionFactory;// настройка и работа с сессиями (фабрика сессий)
+    // настройка и работа с сессиями (фабрика сессий)
+    private static SessionFactory sessionFactory;
 
     static {
-        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()// получение реестра сервисов (что такое Service - http://docs.jboss.org/hibernate/orm/5.1/integrationsGuide/html_single/)
+
+        // получение реестра сервисов (что такое Service - http://docs.jboss.org/hibernate/orm/5.1/integrationsGuide/html_single/)
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure() // настройки из hibernate.cfg.xml
                 .build();
         try {
             // MetadataSources - для работы с метаданными маппинга объектов
-            sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
-        }
-        catch (Exception e) {
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+
+        } catch (Exception e) {
             // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
             // so destroy it manually.
-            StandardServiceRegistryBuilder.destroy( registry );
+            System.err.println("HibernateUtil не смог!!!");
+            System.err.println("HibernateUtil не смог!!!");
+            System.err.println("HibernateUtil не смог!!!");
+           // StandardServiceRegistryBuilder.destroy(registry);
+            e.printStackTrace();
+
         }
     }
 
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
-
 
 
 }
