e1ddfdc43eb268adfeacca7c2d3ffbce90fb079c
anton <anton.it.pavlov@gmail.com>
2016-10-24 12:02:48 +0300

    commit

--- src/main/java/ru/learn/www/start/Start.java
+++ src/main/java/ru/learn/www/start/Start.java
@@ -5,8 +5,6 @@ import ru.learn.www.utils.HibernateUtil;
 public class Start {
 
     public static void main(String[] args) {
-
         System.out.println(HibernateUtil.getSessionFactory());
-
     }
 }
